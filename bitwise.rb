90122f945ed7106cba3328f3d24b73dafe4fcdbc
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-26 22:07:21 +0700

    bitwise operator

---
+++ bitwise.rb
@@ -0,0 +1,11 @@
+A = 8
+B = 10
+
+puts "A = #{A}"
+puts "B = #{B}"
+puts "A Biner is #{A.to_s(2)}"
+puts "B Biner is #{B.to_s(2)}"
+puts "A & B #{A & B}"
+puts "A | B #{A | B}"
+puts "A ^ B #{A ^ B}"
+puts "~A #{~A}"
---
+++ bitwise_shift.rb
@@ -0,0 +1,7 @@
+A = 8
+
+puts "A = #{A}"
+puts "A >> 1 is #{A >> 1}"
+
+puts "A = #{A}"
+puts "A << 1 is #{A << 1}"
